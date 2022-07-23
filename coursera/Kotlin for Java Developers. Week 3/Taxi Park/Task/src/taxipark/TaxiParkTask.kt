ce5aea9cd7c3d23faf7cde08e357c228696a51b0
Luke Cassidy <luke_cassidy@live.co.uk>
2022-07-23 15:32:50 +0100

    added initial solutions for TaxiPark assignments tasks 1-4

--- coursera/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ coursera/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -4,25 +4,28 @@ package taxipark
  * Task #1. Find all the drivers who performed no trips.
  */
 fun TaxiPark.findFakeDrivers(): Set<Driver> =
-        TODO()
+    this.allDrivers.filter { driver -> this.trips.none { it.driver == driver } }.toSet()
 
 /*
  * Task #2. Find all the clients who completed at least the given number of trips.
  */
 fun TaxiPark.findFaithfulPassengers(minTrips: Int): Set<Passenger> =
-        TODO()
+    this.allPassengers.filter { passenger -> trips.count { it.passengers.contains(passenger) } >= minTrips }.toSet()
 
 /*
  * Task #3. Find all the passengers, who were taken by a given driver more than once.
  */
 fun TaxiPark.findFrequentPassengers(driver: Driver): Set<Passenger> =
-        TODO()
+    this.allPassengers.filter { passenger -> this.trips.count { it.driver == driver && it.passengers.contains(passenger) } > 1 }.toSet()
 
 /*
  * Task #4. Find the passengers who had a discount for majority of their trips.
  */
 fun TaxiPark.findSmartPassengers(): Set<Passenger> =
-        TODO()
+    this.allPassengers.filter { passenger ->
+        this.trips.filter { it.passengers.contains(passenger) }.partition { (it.discount ?: 0.0) > 0.0 }
+            .run { first.size > second.size }
+    }.toSet()
 
 /*
  * Task #5. Find the most frequent trip duration among minute periods 0..9, 10..19, 20..29, and so on.
