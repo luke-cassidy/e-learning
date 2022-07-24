0c8c68c7c02bc5282e8fd7d7791a4e16b284d451
luke-cassidy <luke_cassidy@live.co.uk>
2022-07-24 15:12:36 +0100

    initial solutions for tasks 5 and 6 of TaxiPark assignment

--- coursera/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
+++ coursera/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTask.kt
@@ -32,7 +32,7 @@ fun TaxiPark.findSmartPassengers(): Set<Passenger> =
  * Return any period if many are the most frequent, return `null` if there're no trips.
  */
 fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
-    return TODO()
+    return this.trips.groupBy { it.duration - (it.duration % 10) }.maxBy { (_, value) -> value.size }?.run { key..(key + 9) }
 }
 
 /*
@@ -40,5 +40,20 @@ fun TaxiPark.findTheMostFrequentTripDurationPeriod(): IntRange? {
  * Check whether 20% of the drivers contribute 80% of the income.
  */
 fun TaxiPark.checkParetoPrinciple(): Boolean {
-    TODO()
+    val driverStats = this.trips.groupBy { it.driver }.mapValues { (_, value) -> value.sumByDouble { it.cost } }
+        .toList().sortedBy { -it.second }
+    val eightyPercentCost = driverStats.sumByDouble { it.second } * 0.8
+    val twentyPercentDriver = this.allDrivers.size * 0.2
+    var costRunningTotal = 0.0
+    driverStats.forEachIndexed { index, pair ->
+        if (index + 1 <= twentyPercentDriver) {
+            costRunningTotal += pair.second
+            if (costRunningTotal >= eightyPercentCost) {
+                return true
+            }
+        } else {
+            return false
+        }
+    }
+    return false
 }
