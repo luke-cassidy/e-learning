4170600b7efcd0ec07d77e5160d346cbc8be493c
Luke Cassidy <luke_cassidy@live.co.uk>
2022-07-24 20:44:27 +0100

    added optimal solution for TaxiPark task 5

--- coursera/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTaskOptimal.kt
+++ coursera/Kotlin for Java Developers. Week 3/Taxi Park/Task/src/taxipark/TaxiParkTaskOptimal.kt
@@ -32,8 +32,8 @@ fun TaxiPark.findFrequentPassengersOptimal(driver: Driver): Set<Passenger> =
  */
 fun TaxiPark.findSmartPassengersOptimal(): Set<Passenger> =
     this.allPassengers
-        .associate { passenger ->
-            passenger to this.trips.filter { passenger in it.passengers }
+        .associateWith { passenger ->
+            this.trips.filter { passenger in it.passengers }
         }.filterValues { group ->
             val (withDiscount, withoutDiscount) = group.partition { it.discount != null }
             withDiscount.size > withoutDiscount.size
@@ -60,20 +60,59 @@ fun TaxiPark.findTheMostFrequentTripDurationPeriodOptimal(): IntRange? {
  * Check whether 20% of the drivers contribute 80% of the income.
  */
 fun TaxiPark.checkParetoPrincipleOptimal(): Boolean {
-    val driverStats = this.trips.groupBy { it.driver }.mapValues { (_, value) -> value.sumByDouble { it.cost } }
-        .toList().sortedBy { -it.second }
-    val eightyPercentCost = driverStats.sumByDouble { it.second } * 0.8
-    val twentyPercentDriver = this.allDrivers.size * 0.2
-    var costRunningTotal = 0.0
-    driverStats.forEachIndexed { index, pair ->
-        if (index + 1 <= twentyPercentDriver) {
-            costRunningTotal += pair.second
-            if (costRunningTotal >= eightyPercentCost) {
-                return true
-            }
-        } else {
-            return false
-        }
-    }
-    return false
+    if (trips.isEmpty()) return false
+
+    val totalIncome = trips.sumByDouble(Trip::cost)
+    val sortedDriversIncome: List<Double> = trips
+        .groupBy(Trip::driver)
+        .map { (_, tripsByDriver) -> tripsByDriver.sumByDouble(Trip::cost) }
+        .sortedDescending()
+
+    val numberOfTopDrivers = (0.2 * allDrivers.size).toInt()
+    val incomeByTopDrivers = sortedDriversIncome
+        .take(numberOfTopDrivers)
+        .sum()
+
+    return incomeByTopDrivers >= 0.8 * totalIncome
+}
+
+fun main(args: Array<String>) {
+    taxiPark(
+        1..5, 1..4,
+        trip(1, 1, 20, 20.0),
+        trip(1, 2, 20, 20.0),
+        trip(1, 3, 20, 20.0),
+        trip(1, 4, 20, 20.0),
+        trip(2, 1, 20, 19.0)
+    )
+        .checkParetoPrinciple() eq true
+
+    taxiPark(
+        1..5, 1..4,
+        trip(1, 1, 20, 20.0),
+        trip(1, 2, 20, 20.0),
+        trip(1, 3, 20, 20.0),
+        trip(1, 4, 20, 20.0),
+        trip(2, 1, 20, 21.0)
+    )
+        .checkParetoPrinciple() eq false
+}
+
+fun driver(i: Int) = Driver("D-$i")
+fun passenger(i: Int) = Passenger("P-$i")
+fun drivers(range: IntRange) = range.toList().map(::driver).toSet()
+
+fun passengers(indices: List<Int>) = indices.map(::passenger).toSet()
+fun passengers(range: IntRange) = passengers(range.toList())
+fun passengers(vararg indices: Int) = passengers(indices.toList())
+
+fun taxiPark(driverIndexes: IntRange, passengerIndexes: IntRange, vararg trips: Trip) =
+    TaxiPark(drivers(driverIndexes), passengers(passengerIndexes), trips.toList())
+
+fun trip(driverIndex: Int, passenger: Int, duration: Int = 10, distance: Double = 3.0, discount: Double? = null) =
+    Trip(driver(driverIndex), passengers(passenger), duration, distance, discount)
+
+infix fun <T> T.eq(other: T) {
+    if (this == other) println("OK")
+    else println("Error: $this != $other")
 }
