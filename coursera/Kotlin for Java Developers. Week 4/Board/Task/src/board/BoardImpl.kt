c45f1d514ba9ad176eb3d82d048a0f3fa087c541
luke-cassidy <luke_cassidy@live.co.uk>
2022-07-31 17:08:20 +0100

    started Kotlin Week 4 Board assignment, finished SquareBoard

--- coursera/Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
+++ coursera/Kotlin for Java Developers. Week 4/Board/Task/src/board/BoardImpl.kt
@@ -1,7 +1,62 @@
 package board
 
-import board.Direction.*
-
-fun createSquareBoard(width: Int): SquareBoard = TODO()
+fun createSquareBoard(width: Int): SquareBoard = SquareBoardImpl(width)
 fun <T> createGameBoard(width: Int): GameBoard<T> = TODO()
 
+class SquareBoardImpl(override val width: Int) : SquareBoard {
+    private val cells: Array<Array<Cell>> = Array(width) { i -> Array(width) { j -> Cell(i + 1, j + 1) } }
+
+    override fun getCellOrNull(i: Int, j: Int): Cell? {
+        return try {
+            this.cells[i - 1][j - 1]
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            null
+        }
+    }
+
+    override fun getCell(i: Int, j: Int): Cell {
+        return try {
+            this.cells[i - 1][j - 1]
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            throw IllegalArgumentException(e)
+        }
+    }
+
+    override fun getAllCells(): Collection<Cell> {
+        return this.cells.flatten()
+    }
+
+    override fun getRow(i: Int, jRange: IntProgression): List<Cell> {
+        return jRange.mapNotNull {
+            try {
+                this.cells[i - 1][it - 1]
+            } catch (e: ArrayIndexOutOfBoundsException) {
+                null
+            }
+        }
+    }
+
+    override fun getColumn(iRange: IntProgression, j: Int): List<Cell> {
+        return iRange.mapNotNull {
+            try {
+                this.cells[it - 1][j - 1]
+            } catch (e: ArrayIndexOutOfBoundsException) {
+                null
+            }
+        }
+    }
+
+    override fun Cell.getNeighbour(direction: Direction): Cell? {
+        return try {
+            when (direction) {
+                Direction.UP -> this@SquareBoardImpl.cells[i - 2][j - 1]
+                Direction.DOWN -> this@SquareBoardImpl.cells[i][j - 1]
+                Direction.LEFT -> this@SquareBoardImpl.cells[i - 1][j - 2]
+                Direction.RIGHT -> this@SquareBoardImpl.cells[i - 1][j]
+            }
+        } catch (e: ArrayIndexOutOfBoundsException) {
+            null
+        }
+    }
+
+}
